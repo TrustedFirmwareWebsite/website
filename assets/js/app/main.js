8d194a6d22a45a9ba92f0afce5da88e3e3ece986
Kyle Kirkby <kyle.kirkby@linaro.org>
2022-02-04 11:27:01 +0000

    Adding getRandom helper function

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -202,6 +202,19 @@ $(document).ready(function () {
       }
     }
   });
+  function getRandom(arr, n) {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+  }
 
   // Other Posts
   if ($("#other-posts-section").length > 0) {
